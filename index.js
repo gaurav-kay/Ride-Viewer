62af59cc1ca6c8e6b3ceb0a263ea2e82f1783d2c
gaurav-kay <gauravandkashyap@gmail.com>
2023-08-18 12:26:52 +0530

    multiple runs

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ let map;
 let isStarted = false;
 let marker;
 let rider;
-let lats 
-let lons 
-let eles 
-let times 
+let lats
+let lons
+let eles
+let times
 let timestamps
 
 window.addEventListener("load", (event) => {
@@ -14,23 +14,19 @@ window.addEventListener("load", (event) => {
     map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/streets-v12',
-        center: {lat: 20.5937, lon: 78.9629},
+        center: { lat: 20.5937, lon: 78.9629 },
         zoom: 3
     })
 })
 
 function loadMap(fileContent) {
-    // map = new mapboxgl.Map({
-    //     container: 'map',
-    //     style: 'mapbox://styles/mapbox/streets-v12',
-    // })  // making this map object again may cause problems, ideally we should use same map object throughout i think
 
     var gpx = new gpxParser()
     gpx.parse(fileContent)
     let geoJSON = gpx.toGeoJSON();
 
     const points = gpx.tracks[0].points
-    
+
     lats = []
     lons = []
     eles = []
@@ -47,15 +43,13 @@ function loadMap(fileContent) {
     let latWidth = Math.max(...lats) - Math.min(...lats)
     let lonWidth = Math.max(...lons) - Math.min(...lons)
     let bottomLeft = {
-        lat: (Math.min(...lats) - 0.2 * latWidth), 
+        lat: (Math.min(...lats) - 0.2 * latWidth),
         lon: (Math.min(...lons) - 0.2 * lonWidth)
     }
     let topRight = {
-        lat: (Math.max(...lats) + 0.2 * latWidth), 
+        lat: (Math.max(...lats) + 0.2 * latWidth),
         lon: (Math.max(...lons) + 0.2 * lonWidth)
     }
-
-    console.log(bottomLeft, topRight)
 
     if (!map) {
         console.log("Map not initialised yet")
@@ -63,15 +57,22 @@ function loadMap(fileContent) {
 
     map.fitBounds([bottomLeft, topRight])
 
-    map.addSource('route', {
+    if (map.getLayer('routeLayer')) {
+        map.removeLayer('routeLayer')
+    }
+    if (map.getSource('routeSource')) {
+        map.removeSource('routeSource')
+    }
+
+    map.addSource('routeSource', {
         'type': 'geojson',
         'data': geoJSON
     })
 
     map.addLayer({
-        'id': 'route',
+        'id': 'routeLayer',
         'type': 'line',
-        'source': 'route',
+        'source': 'routeSource',
         'layout': {
             'line-join': 'round',
             'line-cap': 'round'
@@ -96,16 +97,16 @@ function setupRide() {
 
     rider = document.createElement('img')
     rider.id = "riderId"
-//     rider.innerHTML = `
-//     <div style="width: 50px; height: auto;">
-//     <svg
-//      version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 365 560" enable-background="new 0 0 365 560" xml:space="preserve" style="width: 100%; height: 100%;">
-//         <g>
-//             <path fill="#00AEEF" d="M182.9,551.7c0,0.1,0.2,0.3,0.2,0.3S358.3,283,358.3,194.6c0-130.1-88.8-186.7-175.4-186.9   C96.3,7.9,7.5,64.5,7.5,194.6c0,88.4,175.3,357.4,175.3,357.4S182.9,551.7,182.9,551.7z M122.2,187.2c0-33.6,27.2-60.8,60.8-60.8   c33.6,0,60.8,27.2,60.8,60.8S216.5,248,182.9,248C149.4,248,122.2,220.8,122.2,187.2z"/>
-//         </g>
-//     </svg>
-// </div>
-//    `
+    //     rider.innerHTML = `
+    //     <div style="width: 50px; height: auto;">
+    //     <svg
+    //      version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 365 560" enable-background="new 0 0 365 560" xml:space="preserve" style="width: 100%; height: 100%;">
+    //         <g>
+    //             <path fill="#00AEEF" d="M182.9,551.7c0,0.1,0.2,0.3,0.2,0.3S358.3,283,358.3,194.6c0-130.1-88.8-186.7-175.4-186.9   C96.3,7.9,7.5,64.5,7.5,194.6c0,88.4,175.3,357.4,175.3,357.4S182.9,551.7,182.9,551.7z M122.2,187.2c0-33.6,27.2-60.8,60.8-60.8   c33.6,0,60.8,27.2,60.8,60.8S216.5,248,182.9,248C149.4,248,122.2,220.8,122.2,187.2z"/>
+    //         </g>
+    //     </svg>
+    // </div>
+    //    `
     rider.style.backgroundImage = `url(https://placekitten.com/g/50/50/)`;
     rider.style.width = `50px`;
     rider.style.height = `50px`;
@@ -117,9 +118,13 @@ function setupRide() {
     // rider.style.width = `50px`;
     // rider.style.height = `50px`;
 
+    if (marker) {
+        marker.remove()
+    }
     marker = new mapboxgl.Marker(rider)
-    .setLngLat({lat: lats[0], lon: lons[0]})
-    .addTo(map);
+        .setLngLat({ lat: lats[0], lon: lons[0] })
+        .addTo(map);
+
 }
 
 async function readFileAsText(file) {
@@ -140,13 +145,13 @@ async function readFileAsText(file) {
 }
 
 document.getElementById('fileInput').addEventListener('change', async (event) => {
-    const selectedFile = event.target.files[0];
+    let selectedFile = event.target.files[0];
 
     if (selectedFile) {
         try {
             document.getElementById("fileSelectedName").innerText = selectedFile.name
 
-            const fileContent = await readFileAsText(selectedFile);
+            let fileContent = await readFileAsText(selectedFile);
             // const fileContent = await fetch('C:\\Projects\\RideViewer\\2023-08-14_14_Aug_2023_4_03_33_pm.gpx')
             // fileContent = fileContent.text()
             loadMap(fileContent)
@@ -168,14 +173,13 @@ document.getElementById('fileInput').addEventListener('change', async (event) =>
 document.getElementById('speedRange').addEventListener('input', (event) => {
     let timestamp = event.target.value
 
-    let closestTimestampIdx = 0 
+    let closestTimestampIdx = 0
     while (timestamps[closestTimestampIdx] < timestamp) {
         closestTimestampIdx += 1
     }
-    console.log("closest timestamp index", closestTimestampIdx)
 
-    marker.setLngLat({lat: lats[closestTimestampIdx], lon: lons[closestTimestampIdx]})
-    .addTo(map);
+    marker.setLngLat({ lat: lats[closestTimestampIdx], lon: lons[closestTimestampIdx] })
+        .addTo(map);
 })
 
 
